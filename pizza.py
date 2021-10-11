5cb8b5062b279149a1f9915458bc5c593e152537
vnsvakanda <phinyaphak.c@ku.th>
2021-10-11 11:54:31 +0700

    Refactoring

--- pizza.py
+++ pizza.py
@@ -1,24 +1,46 @@
+from enum import Enum
+
+
+class PizzaSize(Enum):
+    # Static members of this enumeration
+    small = {'base_price': 120, 'topping': 20}
+    medium = {'base_price': 200, 'topping': 25}
+    large = {'base_price': 320, 'topping': 30}
+    jumbo = {'base_price': 450, 'topping': 35}
+
+    @property
+    def price(self):
+        return self.value['base_price']
+
+    @property
+    def topping_price(self):
+        return self.value['topping']
+
+
 class Pizza:
     """A pizza with a size and optional toppings."""
 
-    def __init__(self, size):
+    def __init__(self, size: PizzaSize):
+        if not isinstance(size, PizzaSize):
+            raise TypeError("size must be a PizzaSize")
         self.size = size
         self.toppings = []
 
-    def getPrice(self):
+    def get_price(self):
         """Price of pizza depends on size and number of toppings."""
-        if self.size == 'small':
-            price = 120 + 20*len(self.toppings)
-        elif self.size == 'medium':
-            price = 200 + 20*len(self.toppings)
-        elif self.size == 'large':
-            price = 280 + 20*len(self.toppings)
-        else:
-            raise ValueError("Unknown pizza size "+self.size)
-        return price
-    
-    def addTopping(self, topping):
+        return self.size.price + self.size.topping_price*len(self.toppings)
+
+    def add_topping(self, topping):
         """Add a topping to the pizza"""
         if topping not in self.toppings:
             self.toppings.append(topping)
 
+    def __str__(self):
+        # create printable description of the pizza such as
+        # "small pizza with mushroom" or "small plain pizza"
+        description = self.size.name
+        if self.toppings:
+            description += " pizza with " + ", ".join(self.toppings)
+        else:
+            description += " plain cheeze pizza"
+        return description
--- pizzashop.py
+++ pizzashop.py
@@ -1,39 +1,40 @@
-from pizza import Pizza
+from pizza import Pizza, PizzaSize
+import pizza
+
 
 # This function shows a limitation on tool-assisted
 # refactoring in a dynamic language like Python.
 #
-# When you rename the Pizza getPrice method to get_price,
+# When you rename the Pizza get_price method to get_price,
 # does it rename the method here?
 # - if no type annotation on the pizza parameter, maybe not
 # - if use type annotation ':Pizza' on the parameter, it should
 
-def order_pizza(pizza):
+def order_pizza(pizza: Pizza):
     """Print a description of a pizza, along with its price."""
 
-    # create printable description of the pizza such as
-    # "small pizza with muschroom" or "small plain pizza"
-    description = pizza.size
-    if pizza.toppings:
-        description += " pizza with "+ ", ".join(pizza.toppings)
-    else:
-        description += " plain cheeze pizza"
-    print(f"A {description}")
-    print("Price:", pizza.getPrice())
+    # print "A small pizza with seafood, mushroom"
+    print(f"A {pizza}")
+    print("Price:", pizza.get_price())
 
 
 if __name__ == "__main__":
-    pizza = Pizza('small')
-    pizza.addTopping("mushroom")
-    pizza.addTopping("tomato")
-    pizza.addTopping("pinapple")
-    order_pizza(pizza)
+    pizza1 = Pizza(PizzaSize.small)
+    pizza1.add_topping("mushroom")
+    pizza1.add_topping("tomato")
+    pizza1.add_topping("pineapple")
+    order_pizza(pizza1)
 
     # a plain pizza
-    pizza2 = Pizza("medium")
+    pizza2 = Pizza(PizzaSize.medium)
     order_pizza(pizza2)
 
     # pizza with only one topping
-    pizza3 = Pizza("large")
-    pizza3.addTopping("seafood")
+    pizza3 = Pizza(PizzaSize.large)
+    pizza3.add_topping("seafood")
     order_pizza(pizza3)
+
+    # pizza with only one topping
+    pizza4 = Pizza(PizzaSize.jumbo)
+    pizza4.add_topping("veggie")
+    order_pizza(pizza4)
